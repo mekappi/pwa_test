6fe5bdc973f878c15a3ed4ac47f2fdb25d9e875c
mekappi <mekappi@gmail.com>
2018-07-23 12:23:41 +0900

    error resolve

--- sw.js
+++ sw.js
@@ -1,15 +1,15 @@
 var CACHE_NAME = 'my-site-cache-v1.00';
 var urlsToCache = [
   '/',
-  '/manifest.json',
-  '/images/icons/icon-72x72.png',
-  '/images/icons/icon-96x96.png',
-  '/images/icons/icon-128x128.png',
-  '/images/icons/icon-144x144.png',
-  '/images/icons/icon-152x152.png',
-  '/images/icons/icon-192x192.png',
-  '/images/icons/icon-384x384.png',
-  '/images/icons/icon-512x512.png'
+  './manifest.json',
+  './images/icons/icon-72x72.png',
+  './images/icons/icon-96x96.png',
+  './images/icons/icon-128x128.png',
+  './images/icons/icon-144x144.png',
+  './images/icons/icon-152x152.png',
+  './images/icons/icon-192x192.png',
+  './images/icons/icon-384x384.png',
+  './images/icons/icon-512x512.png'
 ];
 
 // アプリケーションがインストールされた際に発生します。
